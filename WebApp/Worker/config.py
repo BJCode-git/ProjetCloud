19a48de404255c44faeb3416992dba2664bdf3b6
Brelot Julien <julienb221@gmail.com>
2024-05-12 12:48:56 +0200

    Add App DockerFile

--- WebApp/Worker/config.py
+++ WebApp/Worker/config.py
@@ -5,19 +5,20 @@ import os
 celery = {
     # Transport used to send messages
     # See https://docs.celeryq.dev/en/stable/userguide/configuration.html#broker-url
-    "broker_url": "redis://localhost:6379/0"
+    "broker_url": os.environ.get("CELERY_BROKER_URL","redis://localhost:6379/0"),
     
 }
 
 # Configuration passed to boto3
 # See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.html#boto3.session.Session.resource
 s3 = {
-    "endpoint_url": os.environ.get("S3_ENDPOINT_URL"),
-    "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
-    "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    "endpoint_url": os.environ.get("S3_ENDPOINT_URL", "https://s3.100do.se/"),
+    "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID", "perle"),
+    "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY","GEW3SgnHTJcKC1oTiDlV"),
     "region_name": os.environ.get("AWS_REGION_NAME", "us-east-1"),
     
 }
 
+
 # Name of the bucket used for uploading images
-bucket_name = "images"
+bucket_name = os.environ.get("S3_BUCKET_NAME", "images")
